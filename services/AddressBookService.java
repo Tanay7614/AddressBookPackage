2b1d293edf27bc76a33b7c065a6bce1cf46ef66d
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2022-01-21 11:58:12 +0530

    Taking arguments from Controller Class and pass this parameter to Repository Class.

--- services/AddressBookService.java
+++ services/AddressBookService.java
@@ -7,36 +7,33 @@ public class AddressBookService {
    
     private AddressBookRepository addressBookRepository = new AddressBookRepository();
 	
-    public void addContactService(Person person) 
-    {
+        public void addContactService(Person person) 
+        {
 		 addressBookRepository.saveContact(person);	
 	}
 
-    public List<Person> viewContactService()
-    {
+        public List<Person> viewContactService()
+        {
 		return addressBookRepository.viewContactRepo();
 	}
   
-    public int updateService(Person person, String name) 
-    {
-	     int result=addressBookRepository.updateRepository(person,name);
-	     return result;
-    }
-	
+    
+	public int updateService(String name, String lastName) 
+	{
+		int result=addressBookRepository.updateRepository(name,lastName);
+		return result;
+	}
+
+	public int updateService(int ch,String name) {
+		int r=addressBookRepository.updateRepository(ch,name);
+		return r;
+	}
 
 	public int deleteService(Person person, String name)
 	{
-		 		
 		int result=addressBookRepository.deleteData(person,name);
 		return result;
-		
 	}
 
-	public int updateService(String data1, Person person) 
-	{
-		int result=addressBookRepository.updateRepository(data1,person);
-		return result;
-		
-	}
-
+	
 }
